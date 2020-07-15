aace340fd10b8a58650879334e3730a7426dee36
unknown <harsharay.ray@gmail.com>
2020-07-15 12:26:06 +0530

    Updates

---
+++ precache-manifest.2da26300334d719cb85b389f4cd53748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b088f2401028edc0259fd922b275c86",
+    "url": "/3dp-invoice/index.html"
+  },
+  {
+    "revision": "c90c0db8b1e429efd14d",
+    "url": "/3dp-invoice/static/css/main.bae8cd26.chunk.css"
+  },
+  {
+    "revision": "97f38ca9a830be58d9da",
+    "url": "/3dp-invoice/static/js/2.f271432c.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/3dp-invoice/static/js/2.f271432c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c90c0db8b1e429efd14d",
+    "url": "/3dp-invoice/static/js/main.40d51e64.chunk.js"
+  },
+  {
+    "revision": "e6b368f372162eb881fe",
+    "url": "/3dp-invoice/static/js/runtime-main.fca1f3e9.js"
+  }
+]);
